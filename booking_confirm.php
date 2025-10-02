dcbf1eddbac0791f082535a969116168b1fa04e6
nayif371-collab <abdulrehmannayif371@gmail.com>
2025-10-02 20:07:23 +0500

    Create booking_confirm.php

---
+++ booking_confirm.php
@@ -0,0 +1,4 @@
+<?php
+// Optional file: could be used if you'd prefer process_booking to redirect here.
+// Not required for the demo since process_booking outputs confirmation inline.
+echo "Use process_booking.php which shows confirmation after booking.";
